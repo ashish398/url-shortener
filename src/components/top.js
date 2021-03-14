4491e81f4956697cf52939a26a54d2cb3e1f35f2
ashish <ashish.ashish.jain591@gmail.com>
2021-03-14 23:40:57 +0530

    working app

--- src/components/top.js
+++ src/components/top.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function top() {
     return (
         <div>
-            <h1>Welcome to the URL Shortener Applicatin</h1>
+            <h1>Welcome to the URL Shortener Application</h1>
             <h4>-Powered by Slash Dgraph</h4>
         </div>
     )
